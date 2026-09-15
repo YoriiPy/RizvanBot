de4d2259dd1e01ab115728e19d1ca2c6fc002e40
YoriiPy <gasanismailov094@gmail.com>
2026-09-15 18:51:06 +0300

    d

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dispatcher = Dispatcher()
 
-    await dispatcher.start_polling(bot)
-    await dispatcher.include_router(broadcast.router)
+    dispatcher.start_polling(bot)
+    dispatcher.include_router(broadcast.router)
 if __name__ == "__main__":
     asyncio.run(main())
     asyncio.run(models.init_db())
