928dc2719ab49f0130a663bdb6901fb78498de2f
YoriiPy <gasanismailov094@gmail.com>
2026-09-15 18:59:09 +0300

    d

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -8,7 +8,7 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_command(message: Message):
-    await requests.add_user(message.from_user.id)
+    await requests.add_user(message.from_user.id, message.from_user.first_name)
     await message.answer("🦍 Вас приветствует бот Ризвана\n"
                          "💫 Я сообщу когда начнется стрим\n"
                          "🚀 Вы не пропустите ни одного стрима")
