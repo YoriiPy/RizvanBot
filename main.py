44a157d55e87328c55dbefa841716b5cf1f439ce
YoriiPy <gasanismailov094@gmail.com>
2026-09-15 18:53:39 +0300

    d

--- main.py
+++ main.py
@@ -11,13 +11,22 @@ import asyncio
 file = load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-async def main():
 
+async def main():
+    # 1. Сначала запускаем базу данных и обновляем ссылки в клавиатурах
+    await models.init_db()
+    await user_keyboards.get_url()
+
+    # 2. Инициализируем бота
     bot = Bot(token=BOT_TOKEN)
     dispatcher = Dispatcher()
 
-    await dispatcher.start_polling(bot)
     dispatcher.include_router(broadcast.router)
+
+    # 3. Запускаем бесконечный опрос серверов Telegram
+    await dispatcher.start_polling(bot)
+
+
 if __name__ == "__main__":
     asyncio.run(main())
     asyncio.run(models.init_db())
