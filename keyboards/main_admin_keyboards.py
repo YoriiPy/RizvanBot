ef37f993d5f20ea3f63b4e89e8594568034494d8
YoriiPy <gasanismailov094@gmail.com>
2026-09-16 16:11:57 +0300

    fix dynamic keyboard and clean main entrypoint

--- keyboards/main_admin_keyboards.py
+++ keyboards/main_admin_keyboards.py
@@ -3,7 +3,9 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def start_keyboard():
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text="🚀 Поменять значение URL", callback_data="edit_stream_url")
+    return keyboard.as_markup()
 
 def return_start_keyboard():
     keyboard = InlineKeyboardBuilder()
     keyboard.button(text="⬅️ Назад", callback_data="back_start")
+    return keyboard.as_markup()
