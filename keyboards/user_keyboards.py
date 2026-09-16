8f689fe2db242ac6c7a3fe4bdd24841bfa21b824
YoriiPy <gasanismailov094@gmail.com>
2026-09-16 16:05:54 +0300

    d

--- keyboards/user_keyboards.py
+++ keyboards/user_keyboards.py
@@ -1,6 +1,9 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from database import requests as rq
+import asyncio
 
 def get_url():
+    url = asyncio.run(rq.get_url_stream())
     keyboard = InlineKeyboardBuilder()
-    keyboard.button(text="Стрим 🦍", url=f"{get_url()}")
-    return keyboard
+    keyboard.button(text="Стрим 🦍", url=f"{url}")
+    return keyboard.as_markup()
