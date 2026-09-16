ca16616fe858c8375587c3ce7065999249be39d9
YoriiPy <gasanismailov094@gmail.com>
2026-09-16 18:46:48 +0300

    fix database fetchone nonetype and clear main

--- database/requests.py
+++ database/requests.py
@@ -81,7 +81,7 @@ async def search_admin(user_id: int):
 async def search_main_admin(user_id: int):
     async with aiosqlite.connect(DB_NAME) as db:
 
-        async with db.execute("SELECT 1 FROM users WHERE class = ? AND user_id = ?", ('main_admin', )) as cursor:
+        async with db.execute("SELECT 1 FROM users WHERE class = ? AND user_id = ?", ('main_admin', user_id)) as cursor:
             result = await cursor.fetchone()
             return result
 
