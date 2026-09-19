759dde34562ecfcbfa4201de76c5937a3ace642e
YoriiPy <gasanismailov094@gmail.com>
2026-09-19 18:26:51 +0300

    fix database fetchone nonetype and clear main

--- handlers/main_admin_handlers.py
+++ handlers/main_admin_handlers.py
@@ -78,7 +78,7 @@ async def send_state_stream(callback: CallbackQuery):
         channel_name = rq.get_channel_name()
         await callback.message.edit_text(
                                     "✅ Стрим идет\n"
-                                        f'<b>💫 Ссылка на стрим</b> - <a href="https://www.youtube.com/@{channel_name}/live">Смотреть</a>',
+                                        f'<a href="https://www.youtube.com/@{channel_name}/live">💫 Ссылка на стрим</a>',
                                         reply_markup=keyboard, parse_mode="HTML")
     else:
 
