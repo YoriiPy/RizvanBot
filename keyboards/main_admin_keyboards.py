6a98bdef9ddfbc23a3f8b85ed48c97470dea5d12
YoriiPy <gasanismailov094@gmail.com>
2026-09-20 18:53:19 +0300

    chat + channel

--- keyboards/main_admin_keyboards.py
+++ keyboards/main_admin_keyboards.py
@@ -5,6 +5,7 @@ def start_keyboard():
     keyboard.button(text="🚀 Поменять YouTube канал", callback_data="edit_stream_url")
     keyboard.button(text="👀 Проверить статус эфира", callback_data="check_stream")
     keyboard.button(text="🤖 Поменять Telegram канал", callback_data="edit_tg_channel")
+
     keyboard.adjust(1, 1, 1)
     return keyboard.as_markup()
 
