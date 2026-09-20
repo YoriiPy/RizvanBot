bd39f514d8dd043990b55beb9b292240f38ade69
YoriiPy <gasanismailov094@gmail.com>
2026-09-20 12:18:27 +0300

    Chat + channel

--- bot_broadcast/broadcast.py
+++ bot_broadcast/broadcast.py
@@ -63,7 +63,7 @@ async def broadcast_to_users(bot: Bot):
 
 
         while broadcast and live:
-            message1 = await bot.send_message(chat_id=-1002198546061, text=text, parse_mode="HTML")
+            message1 = await bot.send_message(chat_id=4372478435, text=text, parse_mode="HTML")
             lists.append(message1)
             live = await func.is_live()
             if not live:
