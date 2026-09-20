2202ff193a99e3916c2d7ece105db85b4b0ebc3c
YoriiPy <gasanismailov094@gmail.com>
2026-09-20 13:34:33 +0300

    chat + channel

--- handlers/main_admin_handlers.py
+++ handlers/main_admin_handlers.py
@@ -110,7 +110,7 @@ async def wait_edit_tg_channel(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
     now_tg_channel_id = await rq.get_channel_id()
     await callback.message.edit_text(f"🆕 Отправьте ID нового Telegram канала\n🤖 Текущее ID: {now_tg_channel_id}",
-                                     reply_markup=await kb.return_start_keyboard())
+                                     reply_markup=kb.return_start_keyboard())
     await state.update_data(mid=callback.message.message_id)
     await state.set_state(st.states.wait_new_telegram_channel)
 
